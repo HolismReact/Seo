86de41e6e320f1befd6753246bc99e692fcab5c6
Saeed Nemati <sn.1361@gmail.com>
2022-02-03 15:40:24 +0330

    committed by script

--- Admin/EntityParameters/Upsert.js
+++ Admin/EntityParameters/Upsert.js
@@ -1,8 +1,27 @@
-import { Form } from '@Form'
+import { useState, useEffect } from 'react'
+import { Form, Progress, app, get } from '@Form'
 import Inputs from '../ParameterObject/Inputs'
 
 const UpsertEntityParameter = () => {
-    return <Form
+
+    const { entityType, entityGuid } = app.parseQuery()
+    const [progress, setProgress] = useState()
+
+    useEffect(() => {
+        if (entityType && entityGuid) {
+            setProgress(true)
+            get(`/entityParameter/getRecord?entityType=${entityType}&entityGuid=${entityGuid}`)
+                .then(data => {
+                    app.emit(app.editRequested, { entity: data })
+                    setProgress(false)
+                }, error => {
+                    setProgress(false)
+                    app.error(error)
+                })
+        }
+    }, [])
+
+    return progress ? <Progress /> : <Form
         entityType='EntityParameter'
         inputs={Inputs}
     />
