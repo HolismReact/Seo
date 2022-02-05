d82ce7b1c7a66a4b05dc333827c9bc7a12072344
Saeed Nemati <sn.1361@gmail.com>
2022-02-05 08:50:33 +0330

    committed by script

--- Admin/Node/Tree.js
+++ Admin/Node/Tree.js
@@ -4,6 +4,7 @@ const Nodes = () => {
     return <Tree
         title='Nodes'
         entityType='SnippetNode'
+        show={(node) => `${node.key} ${node.value ? ' - ' + node.value : ''}`}
     />
 }
 
