ce73cb0f94b93cfbe7a9dc70babc82885af6a125
Saeed Nemati <sn.1361@gmail.com>
2022-02-08 18:20:52 +0330

    committed by script

--- Admin/Node/Tree.js
+++ Admin/Node/Tree.js
@@ -1,9 +1,17 @@
-import { Tree } from '@List'
+import { Tree, Text } from '@List'
+
+const filters = <>
+    <Text
+        column='Key'
+        placeholder='Key'
+    />
+</>
 
 const Nodes = () => {
     return <Tree
         title='Nodes'
         entityType='SnippetNode'
+        filters={filters}
         show={(node) => `${node.key} ${node.value ? ' - ' + node.value : ''} ${node.nodeTypeKey === 'JsonObject' ? '{}' : (node.nodeTypeKey === 'JsonArray' ? '[]' : '')}`}
     />
 }
