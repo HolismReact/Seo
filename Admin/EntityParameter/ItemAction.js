36c547fcedd7c6151e6a281a4981e3b6c5d5c4a9
Saeed Nemati <sn.1361@gmail.com>
2022-06-01 09:59:09 +0430

    committed by script

--- Admin/EntityParameter/ItemAction.js
+++ Admin/EntityParameter/ItemAction.js
@@ -1,8 +1,13 @@
 import FindInPageIcon from '@mui/icons-material/FindInPage';
 import { ItemAction } from '@List'
 
-const EntitySeo = ({ entityType, entityGuid }) => {
+const EntitySeo = ({
+    entityType,
+    entityGuid,
+    ...rest
+}) => {
     return <ItemAction
+        {...rest}
         title='Manage SEO'
         icon={FindInPageIcon}
         goTo={`/entityParameter?entityType=${entityType}&entityGuid=${entityGuid}`}
